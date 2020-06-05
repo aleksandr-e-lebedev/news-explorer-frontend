cc7cb65bf9a84fcb468930fae6f6e9e0e7c98fe0
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-06-05 18:00:01 +0300

    Added the 'NoResults.js' BEM block

---
+++ src/blocks/no-results/NoResults.js
@@ -0,0 +1,13 @@
+import ExtendedBaseComponent from '../../js/components/ExtendedBaseComponent';
+
+import './no-results.css';
+
+export default class NoResults extends ExtendedBaseComponent {
+  constructor({
+    selector, domElement, dependencies, container, template,
+  }) {
+    super({
+      selector, domElement, dependencies, container, template,
+    });
+  }
+}
