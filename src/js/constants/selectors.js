d6bbad69fe704866aa15044f2b8048c62324484b
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-06-05 17:30:30 +0300

    Added the 'constants/selectors.js'

---
+++ src/js/constants/selectors.js
@@ -0,0 +1,31 @@
+export default {
+  page: '.page',
+
+  loadingTemplate: '#loading-template',
+  noResultsTemplate: '#no-results-template',
+  requestErrorTemplate: '#request-error-template',
+
+  cardListTemplate: '#card-list-template',
+  cardTemplate: '#card-template',
+
+  popupTemplate: '#popup-template',
+  loginFormTemplate: '#login-form-template',
+  signupFormTemplate: '#signup-form-template',
+  successRegistrationTemplate: '#success-registration-template',
+
+  popup: '.popup',
+  authForm: '.auth-form',
+  successRegistration: '.popup__success-registration',
+
+  header: '.header',
+  searchForm: '.search-form',
+  content: '.content',
+  loading: '.loading',
+  noResults: '.no-results',
+  requestError: '.request-error',
+  cardList: '.card-list',
+  card: '.card',
+
+  summary: '.summary',
+  summaryKeywords: '.summary__keywords',
+};
