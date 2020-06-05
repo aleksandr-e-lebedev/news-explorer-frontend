e0fa87f3b52a6449abd8910c131739959b5d1059
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-06-05 17:40:51 +0300

    Added the 'modules/Storage.js'

---
+++ src/js/modules/Storage.js
@@ -0,0 +1,13 @@
+export default class Storage {
+  static setToken(token) {
+    localStorage.setItem('token', token);
+  }
+
+  static getToken() {
+    return localStorage.getItem('token');
+  }
+
+  static removeToken() {
+    localStorage.removeItem('token');
+  }
+}
